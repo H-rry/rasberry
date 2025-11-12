2706e94706d4cb9668aaf4c77095cdd1bf19287d
Harry Vicaradge <harryvic_@outlook.com>
2025-11-12 20:12:11 +0000

    corrected colour

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return(
-        <footer className="w-full bg-linear-to-r from-fuchsia-500/10 to-rose-500/10 text-white p-4">
+        <footer className="w-full bg-gradient-to-r from-fuchsia-950 via-purple-950 to-rose-950 text-white p-4">
             <div>
                 <h1>
                     hello
